361eee40f4724b42592e77fe7c41c7991c2d0d57
Mickael Ide <ide.mickael@gmail.com>
2020-06-20 00:36:14 +0200

    Fixed NN, added Digits test

--- MLFromScratch/NeuralNetwork/neuralnetwork.py
+++ MLFromScratch/NeuralNetwork/neuralnetwork.py
@@ -1,11 +1,14 @@
 import numpy as np
 from MLFromScratch.Tools import ScoreMulticlass, Softmax, Relu, LeakyRelu, cross_entropy, cross_entropy, derivate
-from MLFromScratch.Tests import testIris
+from MLFromScratch.Tests import testIris, testDigits
 from MLFromScratch.Base import AlgorithmMixin
 
+np.random.seed(0)
+
 class Layer():
-    def __init__(self, size_in, size_out, activation=Relu, bias=True, derivative=None):
-        self.W = np.random.uniform(size=(size_in, size_out), high=0.01, low=-0.01)
+    def __init__(self, size_in, size_out, activation=Relu, bias=True, derivative=None,
+                l1_ratio=0.0, l2_ratio=0.0):
+        self.W = np.random.uniform(size=(size_in, size_out), high=0.1, low=-0.1)
         self.bias = bias
         if self.bias:
             self.B = np.zeros((1, size_out))
@@ -14,6 +17,10 @@ class Layer():
             self.derivative = derivate(activation)
         else:
             self.derivative = derivative
+        self.l1_ratio = l1_ratio
+        self.l2_ratio = l2_ratio
+        self.BGrad = np.zeros_like(self.B)
+        self.WGrad = np.zeros_like(self.W)
 
 
     def forward(self, X):
@@ -25,25 +32,38 @@ class Layer():
         return self.activation(preds)
 
 
-    def backward(self, error, lr):
+    def backward(self, error):
         d = self.derivative(self.preds)
         error *= d
-        grad = self.ins.T.dot(error)
-        next_grad = error.dot(self.W.T)
 
+        l1_W = self.l1_ratio * np.sign(self.W)
+        l2_W = self.l2_ratio * self.W
+        
         if self.bias:
-            self.B -= lr * np.mean(error, 0).reshape((1, -1))
-        self.W -= lr * grad
+            l1_B = self.l1_ratio * np.sign(self.B)
+            l2_B = self.l2_ratio * self.B
+            error = np.mean(error, 0).reshape((1, -1))
+            self.BGrad += error #+ l1_B + l2_B
+            
+        grad = self.ins.T.dot(error)
+        self.WGrad += grad + l1_W + l2_W
+        
+        next_grad = error.dot(self.W.T)
         return next_grad
+
+    def updateGrad(self, lr):
+        self.B -= lr * self.BGrad
+        self.W -= lr * self.WGrad
+        self.BGrad = np.zeros_like(self.B)
+        self.WGrad = np.zeros_like(self.W)
 
 
 class NeuralNetwork(AlgorithmMixin):
-    def __init__(self, layerSizes=[100,], activation=Relu, finalActivation=Softmax, solver="adam", lr=0.01,
+    def __init__(self, layerSizes=[100,], activation=Relu, finalActivation=Softmax, lr=0.01,
                  l1_ratio=0.0, l2_ratio=0.0, n_iters=0, scale=True, bias=True):
         self.layerSizes = layerSizes
         self.activation = activation
         self.finalActivation = finalActivation
-        self.solver = solver
         self.lr = lr
         self.l1_ratio = l1_ratio
         self.l2_ratio = l2_ratio
@@ -53,31 +73,35 @@ class NeuralNetwork(AlgorithmMixin):
         
 
     def fit(self, X, y):
+        EPS = 1e-10
         if self.scale:
             X = np.array(X, dtype=np.float32)
             self.X_offset = np.average(X, axis=0)
             X -= self.X_offset
-            self.X_scale = np.max(X, axis=0)
-            X /= self.X_scale
+            self.X_scale = np.max(np.abs(X), axis=0)
+            X /= (self.X_scale + EPS)
         n_samples, n_features = X.shape
         _, n_classes = y.shape
         
         self.layers = []
         nbIn = n_features
         for nbOut in self.layerSizes:
-            self.layers.append(Layer(nbIn, nbOut, self.activation, self.bias))
+            self.layers.append(Layer(nbIn, nbOut, self.activation, self.bias, l1_ratio=self.l1_ratio, l2_ratio=self.l2_ratio))
             nbIn = nbOut
         derivFinal = lambda x: np.ones_like(x)
-        self.layers.append(Layer(nbIn, n_classes, self.finalActivation, self.bias, derivative=derivFinal))
+        self.layers.append(Layer(nbIn, n_classes, self.finalActivation, self.bias, derivative=derivFinal, l1_ratio=self.l1_ratio, l2_ratio=self.l2_ratio))
         
-
+        
         self.history = []
         for i in range(self.n_iters):
             for j in range(n_samples):
                 preds = self.forward(X[j,None])
                 error = preds - y[j,None]
                 self.backward(error)
-            self.history.append(cross_entropy(y, self.forward(X)))
+            self.updateGrad(self.lr / n_samples)
+            ce = cross_entropy(y, self.forward(X))
+            self.history.append(ce)
+            print(ce)
 
 
 
@@ -90,15 +114,21 @@ class NeuralNetwork(AlgorithmMixin):
 
     def backward(self, errors):
         for layer in reversed(self.layers):
-            errors = layer.backward(errors, self.lr)
+            errors = layer.backward(errors)
         pass
+
+
+    def updateGrad(self, lr):
+        for layer in self.layers:
+            layer.updateGrad(lr)
 
 
 
     def predict(self, X):
+        EPS = 1e-10
         if self.scale:
             X = np.array(X, dtype=np.float32)
-            X = (X - self.X_offset) / self.X_scale
+            X = (X - self.X_offset) / (self.X_scale + EPS)
         preds = X
         for layer in self.layers:
             preds = layer.forward(preds)
@@ -111,4 +141,5 @@ class NeuralNetwork(AlgorithmMixin):
 
 
 if __name__ == "__main__":
-    testIris(NeuralNetwork(layerSizes=[90,20], n_iters=40, lr=0.1))
+    testIris(NeuralNetwork(layerSizes=[20,10], n_iters=100, lr=1, activation=LeakyRelu, l1_ratio=1e-3, l2_ratio=1e-3))
+    testDigits(NeuralNetwork(layerSizes=[60, 20], n_iters=60, lr=1, activation=LeakyRelu))
--- MLFromScratch/Tests/testclassif.py
+++ MLFromScratch/Tests/testclassif.py
@@ -15,3 +15,19 @@ def testIris(algorithm: AlgorithmMixin):
     score = algorithm.score(X_test, y_test)
     print(score.F1Score)
     print((score.F1Score * score.P).sum() / score.P.sum())
+
+
+def testDigits(algorithm: AlgorithmMixin):
+    from sklearn.datasets import load_digits
+    from sklearn.model_selection import train_test_split
+    from sklearn.preprocessing import OneHotEncoder
+    X, y = load_digits(return_X_y=True)
+    enc = OneHotEncoder(handle_unknown='ignore', sparse=False)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    y_train2 = enc.fit_transform(y_train.reshape(-1, 1))
+    
+
+    algorithm.fit(X_train, y_train2)
+    score = algorithm.score(X_test, y_test)
+    print(score.F1Score)
+    print((score.F1Score * score.P).sum() / score.P.sum())
