a01dc16c2bee71ef4e4df553d19c189ae0bd5e7a
Mickael Ide <ide.mickael@gmail.com>
2020-06-23 20:32:25 +0200

    Added KNN

--- MLFromScratch/Tools/tools.py
+++ MLFromScratch/Tools/tools.py
@@ -3,7 +3,7 @@ import numpy as np
 def scale(X):
     EPS = 1e-10
     X = np.array(X, dtype=np.float32)
-    X_offset = np.average(X, axis=0)
+    X_offset = np.mean(X, axis=0)
     X -= X_offset
     X_scale = np.max(np.abs(X), axis=0)
     X /= X_scale + EPS
