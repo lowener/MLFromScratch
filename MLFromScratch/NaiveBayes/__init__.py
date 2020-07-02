93d07d618b9d280175cb0c8c7bd7981ab477b87d
Mickael Ide <ide.mickael@gmail.com>
2020-07-02 03:14:20 +0200

    Added Naive Bayes for #3

---
+++ MLFromScratch/NaiveBayes/__init__.py
@@ -0,0 +1,1 @@
+from .naivebayes import *
---
+++ MLFromScratch/NaiveBayes/naivebayes.py
@@ -0,0 +1,57 @@
+import numpy as np
+from MLFromScratch.Tests import testIris, testBreast, testDigits
+from MLFromScratch.Base import AlgorithmMixin
+from MLFromScratch.Tools import Score, ScoreMulticlass, Sigmoid, Softmax
+
+
+
+class NaiveBayes(AlgorithmMixin):
+    '''
+    Multinomial Naive Bayes
+    References:
+        https://krasserm.github.io/2018/03/19/gaussian-processes/
+        Gaussian Processes for Machine Learning (GPML) by Rasmussen and Williams
+        https://www.youtube.com/watch?v=R-NUdqxKjos
+
+    '''
+    def __init__(self, ):
+        pass
+
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        _, self.n_classes = y.shape
+
+        self.p_classes = np.zeros(self.n_classes)
+        self.mean_class = np.zeros((self.n_classes, n_features))
+        self.variance_class = np.zeros((self.n_classes, n_features))
+
+        for i in range(self.n_classes):
+            self.p_classes[i] = sum(y[:, i]) / n_samples
+            Xi = X[y[:, i] == 1]
+            for j in range(n_features):
+                self.mean_class[i, j] = Xi[:, j].mean()
+                self.variance_class[i, j] = Xi[:, j].var()
+
+    
+    def predict(self, X):
+        EPS=1e-10
+        n_samples, n_features = X.shape
+        y = np.zeros((n_samples, self.n_classes))
+        for i in range(self.n_classes):
+            y[:, i] = self.p_classes[i]
+            for j in range(n_features):
+                y[:, i] *= (1/(EPS + np.sqrt(2 * np.pi * self.variance_class[i, j])))
+                y[:, i] *= np.exp(-(X[:, j] - self.mean_class[i, j])**2 / (EPS + 2 * self.variance_class[i, j]))
+        return y
+
+    
+    def score(self, X, y):
+        preds = self.predict(X).argmax(1)
+        return ScoreMulticlass(y, preds)
+
+
+if __name__ == '__main__':
+    testIris(NaiveBayes())
+    testBreast(NaiveBayes())
+    testDigits(NaiveBayes())
