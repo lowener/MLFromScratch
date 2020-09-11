a43cf6979f0de3f1ff85b6076edd1130aca915e7
Mickael Ide <ide.mickael@gmail.com>
2020-09-11 15:35:23 +0200

    Reworked kmeans to highlight EM steps

--- MLFromScratch/KMeans/kmeans.py
+++ MLFromScratch/KMeans/kmeans.py
@@ -4,15 +4,15 @@ from MLFromScratch.Base import AlgorithmMixin
 from MLFromScratch.Tools import ScoreMulticlass, scale
 
 
-
 class KMeans(AlgorithmMixin):
-    '''
+    """
     K-Means Algorithm
     References:
         Deep Learning, Ian Goodfellow, Section 5.8.2, Page 149
         Pattern Recognition and Machine Learning, Section 9.1, Page 424
 
-    '''
+    """
+
     def __init__(self, K=3, max_iters=1000, scale=False, tol=1e-5):
         self.K = K
         self.scale = scale
@@ -20,44 +20,69 @@ class KMeans(AlgorithmMixin):
         self.tol = tol
         pass
 
-
     def fit(self, X, y=None):
+        """
+        KMeans training follows the Expectation-Maximization algorithm
+        """
         if self.scale:
             X, self.X_offset, self.X_scale = scale(X)
         n_samples, n_features = X.shape
-        
+
         self.centers = np.zeros((self.K, n_features))
 
         # Initialize centers
         for f in range(n_features):
-            self.centers[:, f] = np.linspace(np.quantile(X[:, f], 1/self.K), np.quantile(X[:, f], 1-1/self.K), self.K)
+            self.centers[:, f] = np.linspace(
+                np.quantile(X[:, f], 1 / self.K),
+                np.quantile(X[:, f], 1 - 1 / self.K),
+                self.K,
+            )
 
         self.history = []
         for _ in range(self.max_iters):
-            currentPoints = np.zeros((n_samples, self.K))
             # Expectation
-            for s in range(n_samples):
-                closestCenter = ((self.centers - X[s])**2).sum(1)
-                closestCenter = closestCenter.argmin()
-                currentPoints[s, closestCenter] = 1
+            pointsToCluster = self.e_step(X)
 
             # Maximization
-            points = (currentPoints == 1).argmax(1)
-            for k in range(self.K):
-                if sum(points == k) > 0:
-                    self.centers[k] = X[points == k].mean()
+            centers = self.m_step(X, pointsToCluster)
+            self.centers = centers
 
-            # Logging
+            # Logs and convergence check
             objective = 0
             for s in range(n_samples):
-                objective += (currentPoints[s] * ((self.centers - X[s])**2).sum(1)).sum()
+                objective += (
+                    pointsToCluster[s] * ((self.centers - X[s]) ** 2).sum(1)
+                ).sum()
 
             self.history.append(objective)
             if len(self.history) > 2:
                 if np.abs(self.history[-2] - objective) < self.tol:
                     break
 
-    
+    def e_step(self, X):
+        """
+        Expectation step: each training example is assigned to a cluster
+        """
+        n_samples, n_features = X.shape
+        pointsToCluster = np.zeros((n_samples, self.K))
+        for s in range(n_samples):
+            closestCenter = ((self.centers - X[s]) ** 2).sum(1)
+            closestCenter = closestCenter.argmin()
+            pointsToCluster[s, closestCenter] = 1
+        return pointsToCluster
+
+    def m_step(self, X, pointsToCluster):
+        """
+        Maximization step: each cluster is updated to the mean of training examples assigned to it
+        """
+        n_samples, n_features = X.shape
+        centers = np.zeros((self.K, n_features))
+        points = (pointsToCluster == 1).argmax(1)
+        for k in range(self.K):
+            if sum(points == k) > 0:
+                centers[k] = X[points == k].mean()
+        return centers
+
     def predict(self, X):
         EPS = 1e-10
         n_samples, n_features = X.shape
@@ -67,17 +92,14 @@ class KMeans(AlgorithmMixin):
 
         currentPoints = np.zeros((n_samples, self.K))
         for s in range(n_samples):
-            closestCenter = ((self.centers - X[s])**2).sum(1).argmin()
+            closestCenter = ((self.centers - X[s]) ** 2).sum(1).argmin()
             currentPoints[s, closestCenter] = 1
         return currentPoints.argmax(1)
-
-
-    
 
     def score(self, X, y):
         preds = self.predict(X)
         return ScoreMulticlass(y, preds)
 
 
-if __name__ == '__main__':
-    testBlobs(KMeans(3))
+if __name__ == "__main__":
+    testBlobs(KMeans(3), display=True)
